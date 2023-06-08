6c6c4567fb701ce5a423f78a39a0a07e94d909d6
Pham Quang Trung <mrtrung081099@gmail.com>
2023-06-08 09:03:36 +0700

    add structure project

---
+++ Main.java
@@ -0,0 +1,13 @@
+import services.DictionaryService;
+
+public class Main {
+    private final DictionaryService dictionaryService;
+
+    public Main(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main :");
+    }
+}
---
+++ services/DictionaryServiceImpl.java
@@ -0,0 +1,4 @@
+package services;
+
+public class DictionaryServiceImpl implements DictionaryService{
+}
