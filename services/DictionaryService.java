328b767b10856a71658a1682bc597b536078c345
Pham Quang Trung <mrtrung081099@gmail.com>
2023-06-19 21:59:44 +0700

    add method : random slang

--- services/DictionaryService.java
+++ services/DictionaryService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface DictionaryService {
     void loadDictionaryFromFile(String filePath);
     String getBySlangWord(String slangWord);
+    String randomSlang();
     List<String> getListHistorySearch();
     List<SlangWordInfo> getListByDefinition(String keyDefinition);
     void addSlang(String slang, String definition);
--- services/DictionaryServiceImpl.java
+++ services/DictionaryServiceImpl.java
@@ -8,7 +8,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 public class DictionaryServiceImpl implements DictionaryService{
-    private Map<String, String> dictionary;
+    private final Map<String, String> dictionary;
     private final List<String> searchHistory;
     private final Scanner scanner;
     public DictionaryServiceImpl() {
@@ -44,6 +44,13 @@ public class DictionaryServiceImpl implements DictionaryService{
     }
 
     @Override
+    public String randomSlang() {
+        Random rand = new Random();
+        int indexRandom = rand.nextInt(dictionary.size());
+        return new ArrayList<>(dictionary.keySet()).get(indexRandom);
+    }
+
+    @Override
     public List<String> getListHistorySearch() {
         return searchHistory;
     }
@@ -69,20 +76,18 @@ public class DictionaryServiceImpl implements DictionaryService{
             System.out.println("1. Ghi đè");
             System.out.println("2. Tạo từ mới");
             String choose = scanner.nextLine();
-            switch (choose){
-                case "1":
-                    dictionary.put(slang, definition);
-                    break;
-                case "2":
+            switch (choose) {
+                case "1" -> dictionary.put(slang, definition);
+                case "2" -> {
                     System.out.println("Nhập Slang word mới :");
                     String newKey = scanner.nextLine();
-                    if(!newKey.equalsIgnoreCase(slang) && !dictionary.containsKey(newKey)){
+                    if (!newKey.equalsIgnoreCase(slang) && !dictionary.containsKey(newKey)) {
                         dictionary.put(newKey, definition);
-                    }else {
+                    } else {
                         System.out.println("Slang word này đã có ! Vui lòng thực hiện lại thao tác !");
                         return;
                     }
-                    break;
+                }
             }
         } else {
             dictionary.put(slang, definition);
@@ -112,91 +117,99 @@ public class DictionaryServiceImpl implements DictionaryService{
 
     @Override
     public void run() {
-        while (true) {
-            System.out.println("----- LẬP TRÌNH ỨNG DỤNG JAVA : SLANG WORD -----");
-            System.out.println("----- Các chức năng -----");
-            System.out.println("1. Tìm kiếm theo slang word.");
-            System.out.println("2. Tìm kiếm theo definition.");
-            System.out.println("3. Hiển thị history, danh sách các slang word đã tìm kiếm.");
-            System.out.println("4. Add 1 slang words mới.");
-            System.out.println("5. Edit 1 slang word.");
-            System.out.println("6. Delete 1 slang word.");
-            System.out.println("7. Reset danh sách slang words gốc.");
-            System.out.println("8. Random 1 slang word (On this day slang word.");
-            System.out.println("9. Đố vui: Tìm definition cho slang word.");
-            System.out.println("10. Đố vui: Tìm slang word cho definition.");
-            System.out.println("0. Thoát chương trình.");
-            System.out.print("Nhập lựa chọn của bạn: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice) {
-                case 1:
-                    System.out.println("Nhập Slang word cần tìm kiếm :");
-                    String input1 = scanner.nextLine();
-                    String result1 = getBySlangWord(input1);
-                    if (result1 != null){
-                        System.out.println("Kết quả tìm kiếm : ");
-                        System.out.println(input1+": "+result1);
-                    }else {
-                        System.out.println("Slang word này không có .");
-                    }
-                    break;
-                case 2:
-                    System.out.println("Nhập definition cần tìm kiếm :");
-                    String input2 = scanner.nextLine();
-                    List<SlangWordInfo> result2 = getListByDefinition(input2);
-                    if (result2 != null && !result2.isEmpty()){
-                        System.out.println("Kết quả tìm kiếm : ");
-                        for (SlangWordInfo info : result2){
-                            System.out.println(info.getKey()+": "+info.getValue());
+        try {
+            while (true) {
+                System.out.println("----- LẬP TRÌNH ỨNG DỤNG JAVA : SLANG WORD -----");
+                System.out.println("----- Các chức năng -----");
+                System.out.println("1. Tìm kiếm theo slang word.");
+                System.out.println("2. Tìm kiếm theo definition.");
+                System.out.println("3. Hiển thị history, danh sách các slang word đã tìm kiếm.");
+                System.out.println("4. Add 1 slang words mới.");
+                System.out.println("5. Edit 1 slang word.");
+                System.out.println("6. Delete 1 slang word.");
+                System.out.println("7. Reset danh sách slang words gốc.");
+                System.out.println("8. Random 1 slang word (On this day slang word.");
+                System.out.println("9. Đố vui: Tìm definition cho slang word.");
+                System.out.println("10. Đố vui: Tìm slang word cho definition.");
+                System.out.println("0. Thoát chương trình.");
+                System.out.print("Nhập lựa chọn của bạn: ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 1 -> {
+                        System.out.println("Nhập Slang word cần tìm kiếm :");
+                        String input1 = scanner.nextLine();
+                        String result1 = getBySlangWord(input1);
+                        if (result1 != null) {
+                            System.out.println("Kết quả tìm kiếm : ");
+                            System.out.println(input1 + ": " + result1);
+                        } else {
+                            System.out.println("Slang word này không có .");
                         }
-                    }else {
-                        System.out.println("Không tìm thấy kết quả phù hợp .");
                     }
-                    break;
-                case 3:
-                    System.out.println("Danh sách các slang word đã tìm kiếm :" + getListHistorySearch());
-                    break;
-                case 4:
-                    System.out.println("Nhập slang word mới :");
-                    String slangAdd = scanner.nextLine();
-                    System.out.println("Nhập definition :");
-                    String definitionAdd = scanner.nextLine();
-                    addSlang(slangAdd.toUpperCase(),definitionAdd.toUpperCase());
-                    break;
-                case 5:
-                    System.out.println("Nhập slang word muốn chỉnh sửa :");
-                    String slangEdit = scanner.nextLine().toUpperCase();
-                    if(!dictionary.containsKey(slangEdit)){
-                        System.out.println("Slang word này chưa có, vui lòng thử lại !");
-                    }else {
+                    case 2 -> {
+                        System.out.println("Nhập definition cần tìm kiếm :");
+                        String input2 = scanner.nextLine();
+                        List<SlangWordInfo> result2 = getListByDefinition(input2);
+                        if (result2 != null && !result2.isEmpty()) {
+                            System.out.println("Kết quả tìm kiếm : ");
+                            for (SlangWordInfo info : result2) {
+                                System.out.println(info.getKey() + ": " + info.getValue());
+                            }
+                        } else {
+                            System.out.println("Không tìm thấy kết quả phù hợp .");
+                        }
+                    }
+                    case 3 -> System.out.println("Danh sách các slang word đã tìm kiếm :" + getListHistorySearch());
+                    case 4 -> {
+                        System.out.println("Nhập slang word mới :");
+                        String slangAdd = scanner.nextLine();
                         System.out.println("Nhập definition :");
-                        String definitionEdit = scanner.nextLine();
-                        dictionary.put(slangEdit,definitionEdit);
-                        System.out.println("Kết quả chỉnh sửa : ");
-                        System.out.println(slangEdit+": "+definitionEdit);
+                        String definitionAdd = scanner.nextLine();
+                        addSlang(slangAdd.toUpperCase(), definitionAdd.toUpperCase());
                     }
-                    break;
-                case 6:
-                    System.out.println("Nhập slang word muốn xóa :");
-                    String slangDel = scanner.nextLine().toUpperCase();
-                    delSlang(slangDel);
-                    break;
-                case 7:
-                    System.out.println("Xác nhận reset danh sách slang words gốc :");
-                    System.out.println("1. Reset");
-                    System.out.println("2. Hủy");
-                    String choose = scanner.nextLine();
-                    switch (choose){
-                        case "1":
-                            loadDictionaryFromFile(FilePath.SLANG.url);
-                            System.out.println("Reset thành công !");
-                            break;
-                        case "2":
-                            break;
+                    case 5 -> {
+                        System.out.println("Nhập slang word muốn chỉnh sửa :");
+                        String slangEdit = scanner.nextLine().toUpperCase();
+                        if (!dictionary.containsKey(slangEdit)) {
+                            System.out.println("Slang word này chưa có, vui lòng thử lại !");
+                        } else {
+                            System.out.println("Nhập definition :");
+                            String definitionEdit = scanner.nextLine();
+                            dictionary.put(slangEdit, definitionEdit);
+                            System.out.println("Kết quả chỉnh sửa : ");
+                            System.out.println(slangEdit + ": " + definitionEdit);
+                        }
                     }
-                    break;
+                    case 6 -> {
+                        System.out.println("Nhập slang word muốn xóa :");
+                        String slangDel = scanner.nextLine().toUpperCase();
+                        delSlang(slangDel);
+                    }
+                    case 7 -> {
+                        System.out.println("Xác nhận reset danh sách slang words gốc :");
+                        System.out.println("1. Reset");
+                        System.out.println("2. Hủy");
+                        String choose = scanner.nextLine();
+                        switch (choose) {
+                            case "1":
+                                loadDictionaryFromFile(FilePath.SLANG.url);
+                                System.out.println("Reset thành công !");
+                                break;
+                            case "2":
+                                break;
+                        }
+                    }
+                    case 8 -> {
+                        System.out.println("Slang ngẫu nhiên :");
+                        String slangRandom = randomSlang();
+                        System.out.println(slangRandom + ": " + getBySlangWord(slangRandom));
+                    }
+                }
             }
+        }
+        catch (Exception e){
+            System.out.print(e.getMessage());
         }
     }
 
